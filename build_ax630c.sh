7f89c846c861c24873c322dd1f7d641a124f3b90
祈Inory <584688538@qq.com>
2026-01-19 18:11:16 +0800

    Add scripts and toolchain for axera

---
+++ build_ax630c.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p build_ax630c && cd build_ax630c
+cmake ..  \
+  -DCHIP_AX630C=ON  \
+  -DCMAKE_TOOLCHAIN_FILE=../toolchains/aarch64-none-linux-gnu.toolchain.cmake \
+  -DCMAKE_INSTALL_PREFIX=../install/ax630c \
+  -DCMAKE_BUILD_TYPE=Release  \
+  $@
+make -j4
+make install
