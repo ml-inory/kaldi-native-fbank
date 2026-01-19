72f4d6fee49591f48b048697bb1c82a577bbc59c
祈Inory <584688538@qq.com>
2026-01-19 18:15:26 +0800

    Build static libs

--- build_ax630c.sh
+++ build_ax630c.sh
@@ -5,6 +5,7 @@ cmake ..  \
   -DCMAKE_TOOLCHAIN_FILE=../toolchains/aarch64-none-linux-gnu.toolchain.cmake \
   -DCMAKE_INSTALL_PREFIX=../install/ax630c \
   -DCMAKE_BUILD_TYPE=Release  \
+  -DBUILD_SHARED_LIBS=OFF \
   $@
 make -j4
 make install
